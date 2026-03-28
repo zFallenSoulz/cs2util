efd2e0a71f5060eb9dcb9a85500e7e0b9a47a22d
zFallenSoulz <ryanketel@live.com>
2026-03-27 20:13:46 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CS2Lineups from './CS2Lineups.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CS2Lineups />
+  </React.StrictMode>,
+)
